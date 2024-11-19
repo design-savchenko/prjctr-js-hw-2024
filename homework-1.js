79a7dd686bef5e8e7e789acba934ce9d19ece447
odesign-20 <a.savch94@gmail.com>
2024-11-19 23:34:33 +0200

    init project

---
+++ homework-1.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// First part of Homework
+
+let number1 = 24;
+let number2 = 12;
+
+let finalNumber = "Result: ";
+
+console.log(finalNumber + (number1 + number2));
+console.log(finalNumber + (number1 - number2));
+console.log(finalNumber + number1 * number2);
+
+const base = 24;
+const exponent = 12;
+console.log(Math.pow(base, exponent));
+
+const number = 24;
+console.log(Math.sqrt(number));
+
+// Second part of Homework
+
+let sum1 = 12;
+console.log(String(sum1));
+
+let sum2 = "45";
+console.log(Number(sum2));
+
+console.log(!!sum1);
+console.log(Boolean(sum2));
